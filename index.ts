029a2802ed50a3fa0acea9a93b1ec015f23d284a
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-08-09 12:27:45 +0000

    Refactored some code to make it more readable, introduced a main function and setup everything for the main loop

--- index.ts
+++ index.ts
@@ -5,24 +5,32 @@ type ReviewLevel = {
   nextReview: number;
   score: number;
   userID: number;
-}
+};
 
 type UserData = {
   id: number;
+  knowledgeLevel: number;
   currentReviewLevels: ReviewLevel[];
-}
+};
+
+type Word = {
+  JLPTLevel: number;
+  id: number;
+};
 
 // Global variables
 let userKnowledgeLevel: number = 5;
 let userID: number = 0;
 let currentReviewLevels: ReviewLevel[] = [];
-let allUserData: UserData[] = JSON.parse(await Bun.file("data/userData.json").text());
+let allUserData: UserData[] = JSON.parse(
+  await Bun.file("data/userData.json").text(),
+);
 let userData: UserData;
 
 // Helper functions
-function needsReview(word: { JLPTLevel: number; id: number }) : void {
+function needsReview(word: Word): boolean {
   // Check if the word is in the current review levels
-  let index = search(word.id);
+  let index = searchReviews(word.id);
 
   // If the word is not in the current review levels, add it
   if (index == null) {
@@ -32,22 +40,32 @@ function needsReview(word: { JLPTLevel: number; id: number }) : void {
       score: 0,
       userID: userID,
     });
+
+    return true;
+  } else {
+  }
+
+  return false;
+}
+// Save user data to the file
+function saveUserData(data: UserData) {
+  // Get the index of the user in the allUserData array
+  let index = allUserData.findIndex((user) => user.id == userID);
+
+  // If not found create a new user else update the existing user
+  if (index == -1) {
+    allUserData.push(data);
+    Bun.write("data/userData.json", JSON.stringify(allUserData));
+  } else {
+    allUserData[index] = data;
+    Bun.write("data/userData.json", JSON.stringify(allUserData));
   }
 }
 
-function saveUserData(data: { id: number; currentReviewLevels: ReviewLevel[] }) {
-  // Find the ID of the user in the array
-  // Replace the user data with the new data
-  // Save the new data to the file
-
-  let index = allUserData.findIndex((user) => user.id == userID);
-  allUserData[index] = data;
-  Bun.write("data/userData.json", JSON.stringify(allUserData));
-}
-
-function search(searchId: number) : number | null {
-  let lowerBound : number = 0;
-  let upperBound : number = currentReviewLevels.length - 1;
+// Binary search function for the current review levels
+function searchReviews(searchId: number): number | null {
+  let lowerBound: number = 0;
+  let upperBound: number = currentReviewLevels.length - 1;
   let middle: number;
 
   while (lowerBound < upperBound) {
@@ -67,36 +85,41 @@ function search(searchId: number) : number | null {
   return null;
 }
 
-// Code entry point
+async function loadUser(id: number): Promise<UserData> {
+  // Check if the user ID exists and then save the index
+  const userIndex = allUserData.findIndex((user) => user.id == userID);
 
-process.stdout.write("What is youre ID?\n");
+  if (userIndex != -1) {
+    process.stdout.write("User ID found. Loading user data...\n");
 
-// No clean way to read input from CLI in Bun
-for await (const line of console) {
-  userID = parseInt(line);
-  break;
+    const data = allUserData[userIndex];
+
+    saveUserData(data);
+    return data;
+  } else {
+    process.stdout.write("User ID not found. Creating new user...\n");
+
+    // Get the user's knowledge level
+    userKnowledgeLevel = parseInt(prompt("What is your knowledge level?\n") || "5");
+    
+    const userTemplate: UserData = {
+      id: userID,
+      knowledgeLevel: userKnowledgeLevel,
+      currentReviewLevels: [],
+    }
+
+    // Save user ID to ./data/userData.json and return
+    saveUserData(userTemplate);
+    return userTemplate;
+  }
 }
 
-// Check if the user ID exists and then save the index
-const userIndex: number = allUserData.findIndex((user) => user.id == userID);
+async function main() {
+  /* Code entry point */
 
-if (userIndex != -1) {
-  process.stdout.write("User ID found. Loading user data...\n");
-
-  userData = allUserData[userIndex];
-  currentReviewLevels = userData.currentReviewLevels;
-
-  console.log(userData);
-
-  saveUserData(userData);
-} else {
-  process.stdout.write("User ID not found. Creating new user...\n");
-
-  // Save user ID to ./data/userData.json
-  Bun.write(
-    "data/userData.json",
-    JSON.stringify([...allUserData, { id: userID }]),
-  );
+  // Load user data
+  userData = await loadUser(parseInt(prompt("What is youre ID?\n") || "0"));
+  currentReviewLevels = userData.currentReviewLevels; // Set the current review levels globally
 }
 
-
+main();
